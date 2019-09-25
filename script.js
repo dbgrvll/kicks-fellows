43680220743d68dc04d188736e19fb4672047929
David Gravelle <david.gravelle@gmail.com>
2019-09-25 14:51:51 -0700

    Messaging updates

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ var username = prompt('Welcome! What is your name?');
 console.log(username);
 
 //"concatenate", or add, a variable
-console.log('Hi, ' + username + '!')
+console.log('Hi, ' + username + '! ')
 
-    document.write('Hi, ' + username + '!');
+    document.write('Hi, ' + username + '! ');
 
-var wantsToOrder = prompt('You\'re ready to order your new kicks?');
+var wantsToOrder = prompt(' You\'re ready to order your new kicks? ');
 
 if (wantsToOrder === 'yes') {
 
     document.write(' Glad you\'re ready to order new kicks. ');
 
 } else {
-    document.write('<p>Feel free to browse. Also, ask for any help you need from a consultant.</p>');
+    document.write(' Feel free to browse. Also, ask for any help you need from a consultant. ');
 
 }
